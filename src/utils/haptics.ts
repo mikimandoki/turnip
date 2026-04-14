36a8048eaec5ea6c4cedbf835c75b3b7d19969bb
Miklos Mandoki <miklosmandoki@gmail.com>
2026-04-14 19:33:41 +1000

    use haptics on mobile web

--- src/utils/haptics.ts
+++ src/utils/haptics.ts
@@ -2,12 +2,14 @@ import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 import { isNative } from './utils';
 
+export const canVibrate = 'vibrate' in navigator;
+
 export async function hapticsLight() {
-  if (!isNative) return;
+  if (!isNative && !canVibrate) return;
   await Haptics.impact({ style: ImpactStyle.Light });
 }
 
 export async function hapticsMedium() {
-  if (!isNative) return;
+  if (!isNative && !canVibrate) return;
   await Haptics.impact({ style: ImpactStyle.Medium });
 }
